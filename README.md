45e5d6cd1e0d1d6c7e7bdec4f87dea1f9c1fa11c
Robert Chapman <chapmanrobert12@gmail.com>
2023-09-17 20:36:05 -0500

    Start of project

---
+++ README.md
@@ -0,0 +1,2 @@
+## Any Help
+Those seeking mental health support from a psychiatric hospital can use this web application to find one near them.
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl text-center text-black font-lexend">Welcome to Any Help</h1>
+    </>
+  );
+}
+
+export default App;
