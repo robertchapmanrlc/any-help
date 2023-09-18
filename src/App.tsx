f818665d8b8287d30b9882371a19608eb2f21436
Robert Chapman <chapmanrobert12@gmail.com>
2023-09-17 22:45:33 -0500

    Started the initial part of the welcome page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 function App() {
   return (
-    <>
-      <h1 className="text-3xl text-center text-black font-lexend">Welcome to Any Help</h1>
-    </>
+    <div className="w-full h-screen flex items-center justify-center">
+      <div className="w-full h-full md:h-[70%] md:w-[75%] bg-green-500 md:rounded-3xl flex items-center justify-center">
+        <h1 className="text-3xl md:text-4xl lg:text-5xl text-center text-white font-lexend">
+          Welcome to Any Help
+        </h1>
+      </div>
+    </div>
   );
 }
 
