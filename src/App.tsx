5e5d903e1a3235a9a269a6e4ddb78495e565fe39
Robert Chapman <chapmanrobert12@gmail.com>
2023-09-18 01:11:27 -0500

    Installed framer motion library. Added animations for first part welcome page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
+import { motion } from "framer-motion";
+
 function App() {
   return (
     <div className="w-full h-screen flex items-center justify-center">
-      <div className="w-full h-full md:h-[70%] md:w-[75%] bg-green-500 md:rounded-3xl flex items-center justify-center">
-        <h1 className="text-3xl md:text-4xl lg:text-5xl text-center text-white font-lexend">
+      <motion.div
+        className="w-full h-full md:h-[70%] md:w-[75%] bg-green-500 md:rounded-3xl flex items-center justify-center"
+        initial={{ y: 100, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{
+          delay: 0.2,
+          duration: 0.9
+        }}
+      >
+        <motion.h1
+          className="text-3xl md:text-4xl lg:text-5xl text-center text-white font-lexend"
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0}}
+          transition={{
+            delay: 0.5,
+            duration: 0.9
+          }}
+        >
           Welcome to Any Help
-        </h1>
-      </div>
+        </motion.h1>
+      </motion.div>
     </div>
   );
 }
