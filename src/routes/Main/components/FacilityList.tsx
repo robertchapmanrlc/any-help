d57593328f020a4a84af9e86840bcae169afa955
Robert Chapman <chapmanrobert12@gmail.com>
2023-09-21 01:09:36 -0500

    Added animations for the list of facilities

--- src/routes/Main/components/FacilityList.tsx
+++ src/routes/Main/components/FacilityList.tsx
@@ -1,21 +1,63 @@
+import { data, detail } from "../../../../template.ts";
+import { motion } from "framer-motion";
 
-import { data, detail } from '../../../../template.ts';
+const listVariants = {
+  hidden: {
+    opacity: 0,
+    x: -50,
+  },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      delay: 0.5,
+      duration: 0.7,
+      delayChildren: 1,
+      staggerChildren: 0.05,
+    },
+  },
+};
+
+const facilityVariants = {
+  hidden: {
+    opacity: 0,
+    y: -100,
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 1.5,
+      type: "spring",
+      damping: 10,
+      stiffness: 100,
+    },
+  },
+};
 
 function FacilityList() {
-
   console.log(data.results);
 
   const facilities = data.results;
-  const details = detail.result
+  const details = detail.result;
 
-  return <div className="w-[35%]">
-    {facilities.map((facility) => (
-      <div className='flex flex-col'>
-        <p key={facility.place_id}>{facility.name}</p>
-        <p>{details.website}</p>
-      </div>
-    ))}
-  </div>;
+  return (
+    <motion.div
+      className="w-[35%] flex flex-col items-center align-top gap-5 mt-10"
+      variants={listVariants}
+      initial="hidden"
+      animate="visible"
+    >
+      {facilities.map((facility) => (
+        <motion.div className="flex flex-col" variants={facilityVariants}>
+          <p key={facility.place_id} className="text-center font-lexend">
+            {facility.name}
+          </p>
+          <p className="text-center font-lexend">{details.website}</p>
+        </motion.div>
+      ))}
+    </motion.div>
+  );
 }
 
 export default FacilityList;
